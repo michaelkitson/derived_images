0711fb83177741af4f5b80f581b1eaea7768e689
Michael Kitson <michaelskitson@gmail.com>
2023-05-25 20:00:19 -0700

    Add Manifest#diff_from to detect changes so that we can clean up a cache

--- test/manifest_test.rb
+++ test/manifest_test.rb
@@ -14,11 +14,11 @@ class ManifestTest < ActiveSupport::TestCase
 
   attr_reader :manifest
 
-  def test_draw_from_block
+  test '#draw from a block' do
     assert_equal 3, manifest.count
   end
 
-  def test_draw_from_file
+  test '#draw from a file' do
     Tempfile.create do |file|
       file.write("derive 'a.png', from: 'c.png'\n derive 'b.png', from: 'c.png'\n derive '1.png', from: '2.png'")
       file.rewind
@@ -28,24 +28,25 @@ class ManifestTest < ActiveSupport::TestCase
     assert_equal 3, manifest.count
   end
 
-  def test_produced_from
+  test '#produced_from' do
     entries = manifest.produced_from(Pathname.new('app/assets/images/c.png').expand_path)
     assert_equal 2, entries.length
     assert_equal 'c.png', entries[0].source
     assert_equal 'c.png', entries[1].source
   end
 
-  def test_resize
-    manifest.draw { resize('a.png', from: 'b.png', width: 640, height: 480) }
-    entry = manifest['a.png']
-    assert_equal 'a.png', entry.target
-    assert_equal 'b.png', entry.source
-    assert_equal([:resize_to_limit, [640, 480]], entry.pipeline.options[:operations].sole)
-  end
+  test '#diff_from' do
+    new_manifest = DerivedImages::Manifest.new(nil)
+    new_manifest.draw do
+      resize('a.png', from: 'c.png', width: 1, height: 1) # Changed
+      derive('b.png', from: 'c.png') # Constant
+      # derive('1.png', from: '2.png') # Removed
+      derive('3.png', from: '2.png') # Added
+    end
 
-  def test_derive
-    pipeline = nil
-    manifest.draw { derive('a.png', from: 'b.png') { pipeline = _1 } }
-    assert_kind_of(ImageProcessing::Chainable, pipeline)
+    added, changed, removed = new_manifest.diff_from(manifest)
+    assert_equal '3.png', added.sole.target
+    assert_equal 'a.png', changed.sole.target
+    assert_equal '1.png', removed.sole.target
   end
 end
