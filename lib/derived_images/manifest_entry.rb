5cbf3daa5d106c164410b41ccab55581f7aaaef9
Mike Kitson <michaelkitson@users.noreply.github.com>
2023-05-27 15:35:09 -0700

    Add a patch to fix a bug in older versions of libvips where specifying avif would generate an heic image (#7)

--- lib/derived_images/manifest_entry.rb
+++ lib/derived_images/manifest_entry.rb
@@ -8,10 +8,7 @@ module DerivedImages
     def initialize(source, target)
       @source = source
       @target = target
-      target_ext = File.extname(target)
-      pipeline = PROCESSORS.fetch(DerivedImages.config.processor).dup
-      pipeline = pipeline.convert(target_ext) if target_ext.present?
-      @pipeline = pipeline
+      @pipeline = default_pipeline(File.extname(target))
     end
 
     def ==(other)
@@ -45,6 +42,7 @@ module DerivedImages
     end
 
     PROCESSORS = { mini_magick: ImageProcessing::MiniMagick, vips: ImageProcessing::Vips }.freeze
+    COMPRESSION_OVERRIDES = { 'avif' => 'av1', 'heic' => 'hevc' }.freeze
 
     def target_digest
       Digest::SHA256.file(target_path).hexdigest
@@ -58,6 +56,17 @@ module DerivedImages
 
     def source_digest
       Digest::SHA256.file(source_path).hexdigest
+    end
+
+    def default_pipeline(target_ext)
+      pipeline = PROCESSORS.fetch(DerivedImages.config.processor).dup
+      pipeline = pipeline.convert(target_ext) if target_ext.present?
+      vips = pipeline.options[:processor] == ImageProcessing::Vips::Processor
+      if vips && !Vips.at_least_libvips?(8, 12) && COMPRESSION_OVERRIDES.key?(target_ext)
+        # https://github.com/libvips/libvips/commit/47383b5bfc136f7870c319a1edce4848e177425b
+        pipeline = pipeline.saver(compression: COMPRESSION_OVERRIDES.fetch(target_ext))
+      end
+      pipeline
     end
 
     protected
